0a9fc5551a0f9f34330d0c65e6bef97d1a18b31a
Kareila <kareila@dreamwidth.org>
2013-05-09 09:31:18 -0500

    Initial public release of CLI with basic features.

--- README.md
+++ README.md
@@ -1,2 +1,192 @@
-clean-all-the-things
+Clean All The Things
 ====================
+
+This software does not, alas, clean all the things.  However, it is useful
+for helping one focus on which items are most in need of cleaning, so that
+all the things might be found to be reasonably clean at any given time.
+
+I am a Perl nerd, and I don't expect this software to be used by anyone but
+fellow Perl nerds.  You should at least know how to use CPAN to install the
+requested modules, although you can safely comment out the block containing
+`use Net::Twitter::Lite` if you have no intention of using this app with
+Twitter.
+
+This should be considered an alpha release.  Expect falling rocks, etc.
+
+
+Changes
+-------
+
+### 8 May 2013
+
+Initial release.
+
+
+Overview
+--------
+
+This distribution should contain the following files:
+
+- `README.md`    (these instructions)
+- `clean_cli.pl` (command line interface)
+- `housework.db` (a SQLite database)
+
+Running `clean_cli.pl` will give you command line access to job status and
+the ability to add new jobs or edit existing jobs.  It expects `housework.db`
+to be in the same directory as itself, so keep them together.
+
+The only other filesystem assumption is the existence of a .ssh directory in
+the user's home directory.  If the script is configured to post status updates
+to Twitter (more on that later), it will store the access codes as a Storable
+hash in `$HOME/.ssh/.cleanthings` with user-read-only (600) permissions.  You
+can change this location if you want by editing the value of `$twitter_file`
+in `clean_cli.pl`.
+
+In the near future I hope to add a subdirectory of webpages providing a
+more generally accessible interface to the database, and I'll probably have
+to program it in PHP.  (I'm sorry.)  But you'll still want the command line
+script to run out of cron to do regular maintenance on the database, as
+described below.
+
+
+Usage
+-----
+
+The `clean_cli.pl` script has two major modes.  Without any options, it will
+provide a basic user interface for viewing and editing jobs.  Here's some
+example output:
+
+    11 jobs found, sorted by most needed first.
+
+    01: Clean Kitchen Floor                                          (100%)
+    02: Clean Bathroom Floor Upstairs                                (70%)
+    03: Clean Windowsills and Baseboards                             (70%)
+    04: Clean Bathroom Counter Downstairs                            (70%)
+    05: Vacuum Carpet Upstairs                                       (65%)
+    06: Clean Bathroom Mirrors Upstairs                              (60%)
+    07: Clean Bathroom Sink Upstairs                                 (60%)
+    08: Clean Bathroom Counter Upstairs                              (50%)
+    09: Clean Kitchen Countertops                                    (25%)
+    10: Vacuum Carpet Downstairs                                     (10%)
+    11: Clean Microwave                                              (0%)
+
+    You may (a)dd a new job, (e)dit an existing job, change the (s)ort order, or
+     (q)uit.
+
+    >
+
+The other major mode is maintenance mode, which is invoked by using the
+`--maint` option on the command line.  In maintenance mode, the script will
+examine the database, adjust any urgency percentages that need adjusting, print
+a status message, and exit.  To have the status message sent to a Twitter
+account, run `clean_cli.pl --twitter` and follow the configuration instructions.
+This is a one-time configuration option; once configured, Twitter will be
+automatically used for all future status updates.
+
+Two other command line options are available.  The `--silent` flag suppresses
+all those chatty, helpful status messages and prints only the most essential
+output.  (I recommend running `clean_cli.pl --maint --silent` out of cron if
+Twitter is being used, to avoid getting any email unless an error occurs.)
+The other command line option is `--region=<n>` which only comes into play if
+you assign your jobs into different numbered regions.  For example, I use
+two regions named (1)Upstairs and (2)Downstairs, and I actually have two cron
+jobs defined, such that I get notified of Upstairs jobs in the morning and
+Downstairs jobs in the afternoon, using `--region=1` and `--region=2`
+respectively.
+
+All the options can be abbreviated: `-m`, `-t`, `-s`, and `-r=<n>` will work.
+
+
+Defining A Job
+--------------
+
+    You may (a)dd a new job, (e)dit an existing job, change the (s)ort order, or
+     (q)uit.
+
+    > a
+
+    Currently defined regions:
+      1: Upstairs
+      2: Downstairs
+
+    Enter your choice - 1, 2, or 'n' for new region.
+    [n]> 2
+
+    You may rename this region, or press RETURN to confirm the current name.
+    [Downstairs]>
+
+    New job name: Clean Bathroom Mirror Downstairs
+
+    Number of days between updates: 6
+
+    Increment amount [0%]: 10
+
+    Current urgency [0%]: 0
+
+I've already explained regions, and job name should be self-explanatory.
+But what about the numbers?  Here's the idea: the urgency is how badly the job
+needs to be done, on a scale from 0% (just cleaned) to anything over 100%
+(disgusting).  Since your computer can't actually examine your house to see how
+dirty things are (or mine can't, at any rate), it does some math based on the
+numbers you tell it to use.  Every time you execute the script in maintenance
+mode, it looks at the last time the urgency was changed and if it was N or more
+days ago (N being '6' in the example above), it adds the increment amount (in
+this case, 10%) to the total.  In this fashion the urgency of any given job
+will slowly creep back up to 100% at the rate you describe, until you log in
+to edit the job back down to 0%.  Negative totals are also supported.  Say you
+have a job that says "Buy one (X)" and you find it on sale and buy two, you can
+say the current urgency is -100% and it will cope accordingly.  (Behold the
+power of math!)
+
+
+Bugs
+----
+
+No known bugs, just features I haven't added yet.  There's currently no way to
+mark a job as done without being prompted to edit all the details of the job.
+There's currently no way to delete a job or a region, although you can edit it
+to be something completely different.  There's no way to temporarily suspend a
+job except to edit the frequency or increment amount to zero, and then set it
+back to the desired value when you want it to start running again.  There's no
+way to adjust the warning threshold on the fly; it's hardcoded to notify once a
+job's urgency reaches 80%, although only the descriptions of the top two most
+urgent jobs will be printed in the status message.
+
+The database ships with two regions predefined, Upstairs and Downstairs.  As I
+said above, you can rename them but not delete them from the app.  There's no
+way to turn off the region feature if you don't need it; a job is always
+attached to a region.  Of course, all things are possible if you want to edit
+the script and mess around with sqlite, but you're on your own with that.  :)
+
+I haven't tested this with job names longer than about 40 characters, and the
+status display will probably break if it goes over 60 or so, plus there's the
+140 character limit of Twitter to consider.  I should remember to enforce a
+name length restriction at some point.
+
+There are probably other features that will be obvious to anyone else that I
+just haven't thought of because they don't fit my intended usage of the app.
+Pull requests are welcome!
+
+
+License
+-------
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.  For more information about the Perl Artistic
+License 2.0, visit <http://opensource.org/licenses/artistic-license-2.0>.
+
+
+Disclaimers
+-----------
+
+This software is provided "as is" and any express or implied warranties,
+including, but not limited to, the implied warranties of merchantability and
+fitness for a particular purpose are disclaimed.
+
+The author of this software is in no way affiliated with the website
+[Hyperbole and a Half](http://hyperboleandahalf.blogspot.com/), whose
+"This Is Why I'll Never Be An Adult" web comic inspired the name of this
+application.
+
+
+_kareila at dreamwidth dot org // 8 May 2013_
---
+++ clean_cli.pl
@@ -0,0 +1,412 @@
+#!/usr/bin/perl
+
+# This script is part of the Clean All The Things project, maintained at
+# https://github.com/kareila/clean-all-the-things
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself. For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+
+
+use warnings;
+use strict;
+
+my $region = 0;  # constrain to a certain region (expects integer id)
+my $silent = 0;  # suppress warnings (e.g. when running out of crontab)
+my $maint  = 0;  # flag indicating maintenance mode (calculate updates and exit)
+my $tconf  = 0;  # flag indicating whether Twitter config info needs to be entered
+
+use Getopt::Long;
+GetOptions( 'twitter' => \$tconf, 'maint' => \$maint, 'silent' => \$silent, 'region=i' => \$region );
+
+use Storable;
+my $skip_twitter = 0;
+my $twitter_file = $ENV{"HOME"} . "/.ssh/.cleanthings";
+my $twitter_info;
+
+# check for saved Twitter auth info
+$twitter_info = Storable::retrieve $twitter_file if -f $twitter_file;
+
+if ( $maint && ! defined $twitter_info ) {
+    $skip_twitter = 1;
+    warn "No Twitter access information found; will not post status updates.\n"
+        unless $silent;
+}
+
+use DBI;
+my $dbh = DBI->connect( "dbi:SQLite:housework.db" ) || die "Cannot connect: $DBI::errstr";
+my ( $jobs, %jobs, %jobtimes, @jobtimes, %regnames );
+
+sub reload_jobs {
+    if ( $_[0] ) {  # optionally populates %regnames
+        my $reg = $dbh->selectall_arrayref( 'SELECT * FROM regions', { Slice => {} } );
+        %regnames = map { $_->{regid} => $_->{regname} } @$reg;
+    }
+
+    my $select_jobs = 'SELECT * FROM jobs';
+    $select_jobs .= ' WHERE regid=?' if $region;
+    $select_jobs .= ' ORDER BY currtotal DESC';
+    my @sel_job_args = ( $select_jobs, { Slice => {} } );
+    push @sel_job_args, $region if $region;
+
+    $jobs = $dbh->selectall_arrayref( @sel_job_args );
+    return unless @$jobs;  # nothing else will turn up anything useful
+    %jobs = map { $_->{jobid} => $_ } @$jobs;
+
+# in maintenance mode, we care about the last time the urgency was incremented.
+# in user mode, we care about the last time the urgency was decremented (work done).
+
+    my $select_timelog = 'SELECT MAX(timestamp) FROM timelog WHERE jobid=? AND percent';
+    $select_timelog .= $maint ? '>=0' : '<=0';
+
+    foreach my $job ( @$jobs ) {
+        my $jobid = $job->{jobid} or die 'No jobid found!';
+        my @latest = $dbh->selectrow_array( $select_timelog, undef, $jobid );
+        if ( $latest[0] ) {
+            my $last = $dbh->selectall_arrayref( 'SELECT * FROM timelog WHERE jobid=?'
+                       . ' AND timestamp=?', { Slice => {} }, $jobid, $latest[0] );
+            @latest = @$last;
+        }
+        $jobtimes{$jobid} = $latest[0];
+    }
+
+    @jobtimes = sort { $a->{timestamp} <=> $b->{timestamp} } values %jobtimes;
+
+    # this populates the global job variables; nothing to return
+}
+
+&reload_jobs(!$maint);
+
+if ( $maint ) {
+    exit 0 unless @$jobs;  # nothing to do
+    my %updated;
+
+    # update urgencies as needed
+    foreach my $id ( keys %jobs ) {
+        my $jdata = $jobs{$id};
+        my $tdata = $jobtimes{$id};
+        next unless $jdata->{frequency} > 0;
+        next unless $jdata->{urgency};
+
+        # plus or minus five minutes...
+        if ( time > $jdata->{frequency} * 86100 + $tdata->{timestamp} ) {
+            $updated{$id} = $jdata->{currtotal} + $jdata->{urgency};
+            $dbh->do( 'UPDATE jobs SET currtotal=? WHERE jobid=?',
+                      undef, $updated{$id}, $id );
+            $dbh->do( 'INSERT INTO timelog (jobid, timestamp, percent)' .
+                      ' VALUES (?,?,?)', undef, $id, time, $jdata->{urgency} );
+
+        }
+    }
+
+    if ( %updated ) {
+        &reload_jobs();
+        unless ( $silent ) {
+            foreach my $j ( keys %updated ) {
+                warn sprintf( "Job #%d updated to %d%% needed.\n",
+                              $j, $updated{$j} );
+            }
+            warn "\n";
+        }
+
+        # figure out top two or three most urgent jobs & notify
+        my @j = grep { $_->{currtotal} >= 80 } @$jobs;
+        return unless @j;  # nothing urgent
+
+        my $twitter_status = '';
+        foreach (0..1) {
+            next unless defined $j[$_];
+            $twitter_status .= $j[$_]->{jobname} . ' (';
+            $twitter_status .= $j[$_]->{currtotal} . '%) ';
+        }
+        my $rem = scalar @j - 2;
+        $twitter_status .= "+$rem more" if $rem > 0;
+        chomp $twitter_status;
+
+        if ( $twitter_status && ! $skip_twitter ) {
+            warn "Posting to Twitter using stored credentials.\n"
+                unless $silent;
+            use Net::Twitter::Lite;
+            # "Install Net::OAuth 0.25 or later for OAuth support"
+            use Net::OAuth;
+            my $nt = Net::Twitter::Lite->new( %$twitter_info, legacy_lists_api => 0 );
+            eval { $nt->update( $twitter_status ) };
+            warn "Twitter error: $@\n" if $@;
+        }
+
+        warn "$twitter_status\n" unless $silent;
+
+    } else {
+        warn "No updates.\n" unless $silent;
+    }
+
+    exit 0;
+}
+
+# if we haven't exited by now, we wanted to run in user mode.
+
+use Term::ReadLine;
+my $term = new Term::ReadLine;
+
+warn "Welcome to the Clean All The Things configuration interface.\n\n" .
+     "To run this script in maintenance mode, which notes the current time and\n" .
+     " posts a cleanup list to Twitter (if enabled), use the --maint option.\n\n"
+    unless $silent;
+
+if ( $tconf ) {
+    warn "Please enter your consumer key and access token below.\n" .
+         "If you don't have these, you will need to register on dev.twitter.com.\n" .
+         "Refer to http://dft.ba/-5AhL for more information.\n\n" unless $silent;
+    warn "WARNING: cached Twitter tokens were found!!!\n" .
+         "Proceed only if you wish to enter new tokens!\n" .
+         "Otherwise, rerun this script without the --twitter option.\n\n" if $twitter_info;
+
+    # prompt for Twitter information
+    foreach my $key ( qw( consumer_key consumer_secret access_token access_token_secret ) ) {
+        my $prompt = ucfirst "$key: "; $prompt =~ s/_/ /;
+        $twitter_info->{$key} = $term->readline($prompt);
+        chomp $twitter_info->{$key};
+        exit 0 unless length $twitter_info->{$key};
+    }
+    Storable::store $twitter_info, $twitter_file;
+    chmod 0600, $twitter_file;
+    warn "Twitter information stored in $twitter_file\n\n" unless $silent;
+}
+
+unless ( -f $twitter_file ) {
+    warn "No Twitter access info found; please rerun with --twitter option to configure.\n\n"
+        unless $silent;
+}
+
+# enough about Twitter configuration, let's manage jobs.
+
+use POSIX qw( strftime );
+
+my $limit = 20;
+my $jsort = 0;
+my %jsort = ( '0' => 'most needed', '1' => 'least recent' );
+
+&print_status;
+
+sub print_status {
+    warn "More than $limit matching jobs found; only displaying top $limit.\n"
+        if !$silent and scalar @$jobs > $limit;
+    printf "%d jobs found, sorted by %s first.\n\n", scalar @$jobs, $jsort{$jsort};
+    my @print_jobs = $jsort ? @jobtimes : @$jobs;
+    my $i = 1;
+    foreach my $j ( @print_jobs ) {
+        my $id = $j->{jobid};
+        my $stat = $jsort ? strftime( "%D", localtime( $jobtimes{$id}->{timestamp} ) )
+                          : $j->{currtotal} . '%';
+        printf "%02d: %-60s (%s)\n", $i, $jobs{$id}->{jobname}, $stat;
+        last if $i++ > $limit;
+    }
+    print "\n";
+    &main_prompt();
+}
+
+sub main_prompt {
+    print "You may (a)dd a new job, (e)dit an existing job, change the (s)ort order, or\n (q)uit.\n\n";
+    my $input = &print_prompt();
+    &parse_input($input);
+}
+
+sub print_prompt {
+    my $prompt = defined $_[0] && length $_[0] ? $_[0] : "> ";
+    my $input = $term->readline($prompt);
+    chomp $input;
+    print "\n";
+    return $input;
+}
+
+sub parse_input {
+    my $input = $_[0];
+    my ($c, @args) = split /\s+/, $input;
+    return &main_prompt() unless defined $c and length $c;
+
+    if ( $c =~ /^s/ ) { # change the sort order
+        $jsort = $jsort ? 0 : 1;
+        return &print_status;
+    } elsif ( $c =~ /^q/ ) {
+        exit 0;
+    } elsif ( $c =~ /^a/ ) {
+        return &prompt_add();
+    } elsif ( $c =~ /^e/ ) {
+        return &prompt_edit( @args );
+    } else {
+        print "Sorry, I don't know what you mean by that.\n";
+        return &main_prompt();
+    }
+}
+
+sub prompt_edit {
+    my @args = @_;
+    unless ( @args ) {
+        print "Please type the number of the job you wish to edit.\n\n";
+        my $input = &print_prompt();
+        @args = ($input);
+    }
+    my $j = $args[0];
+    my @print_jobs = $jsort ? @jobtimes : @$jobs;
+    my $job = $print_jobs[$j-1];
+    if ( $j !~ /^\d+$/ or $j > $limit or $j < 1 or ! $job->{jobid} ) {
+        print "Sorry, I can't find that job.\n";
+        return &main_prompt();
+    }
+    &show_details( $job->{jobid} );
+    my $continue = &print_prompt( 'Enter new data for this job? [Y/N] > ' );
+    return &prompt_add( $job->{jobid} ) if $continue && $continue =~ /^y/i;
+    &print_status;
+}
+
+sub show_details {
+    my ( $jobid ) = @_;
+    return unless $jobid;
+    my $jdata = $jobs{$jobid};
+    my $tdata = $jobtimes{$jobid};
+    my $region = $regnames{$jdata->{regid}} . ' (#' . $jdata->{regid} . ')';
+
+    my @lines = (
+                 [ 'Job Name', $jdata->{jobname} ],
+                 [ 'Region Name', $region ],
+                 [ 'Number of days between urgency updates', $jdata->{frequency} ],
+                 [ 'Amount to increment urgency on each update' , $jdata->{urgency} . '%' ],
+                 [ 'Current urgency', ( $jdata->{currtotal} || 0 ) . '%' ],
+                 [ 'Last worked on', strftime( "%D", localtime( $tdata->{timestamp} ) ) ]
+                );
+
+    foreach ( @lines ) {
+        printf "%s: %s\n", $_->[0], $_->[1];
+    }
+}
+
+sub prompt_add {
+    my ( $jobid ) = @_;
+    my $jdata = defined $jobid ? $jobs{$jobid} : {};
+
+    my $regid = &prompt_region( $jdata->{regid} );
+
+    my $append = sub { $_[0] . ( $_[1] ? " \[$_[1]\]" : '' ) . ': ' };
+
+    my $jobname = &print_prompt( $append->( "New job name", $jdata->{jobname} ) );
+    $jobname = $jdata->{jobname} unless length $jobname;
+    return &main_prompt() unless $jobname;
+
+    my $freq = &print_prompt( $append->( "Number of days between updates", $jdata->{frequency} ) );
+    $freq = $jdata->{frequency} || 0 unless length $freq;
+
+    my $urg = &print_prompt( $append->( "Increment amount", ( $jdata->{urgency} || 0 ) . '%' ) );
+    $urg = $jdata->{urgency} || 0 unless length $urg;
+    $urg =~ s/%$// if defined $urg;
+
+    my $curr = &print_prompt( $append->( "Current urgency", ( $jdata->{currtotal} || 0 ) . '%' ) );
+    $curr = $jdata->{currtotal} || 0 unless length $curr;
+    $curr =~ s/%$// if defined $curr;
+
+    my @lines = (
+                 [ 'Job Name', $jobname ],
+                 [ 'Region Name', $regnames{$regid} . ' (#' . $regid . ')' ],
+                 [ 'Number of days between urgency updates', $freq ],
+                 [ 'Amount to increment urgency on each update' , $urg . '%' ],
+                 [ 'Current urgency', $curr . '%' ],
+                );
+
+    foreach ( @lines ) {
+        printf "%s: %s\n", $_->[0], $_->[1];
+    }
+    print "\nIf everything is correct, press Y to continue, or N to abort changes.\n";
+    my $continue = &print_prompt( 'Is everything correct? [Y/N] > ' );
+    return &main_prompt() if $continue && $continue =~ /^n/i;
+
+    if ( defined $jobid ) {
+        $dbh->do( 'UPDATE jobs SET jobname=?, frequency=?, urgency=?, regid=?, currtotal=?' .
+                  ' WHERE jobid=?', undef, $jobname, $freq, $urg, $regid, $curr, $jobid );
+
+    } elsif ( $jobname && $freq =~ /^\d+$/ && $urg =~ /^\d+$/ && $curr =~ /^\d+$/ ) {
+        $dbh->do( 'INSERT INTO jobs (jobname, frequency, urgency, regid, currtotal)' .
+                  ' VALUES (?,?,?,?,?)', undef, $jobname, $freq, $urg, $regid, $curr );
+        ( $jobid ) = $dbh->selectrow_array( 'SELECT jobid FROM jobs WHERE jobname=?',
+                                            undef, $jobname );
+    }
+
+    if ( $jobid ) {
+        my $percent = 0;
+        $percent = $curr - $jdata->{currtotal} if defined $jdata->{currtotal};
+        $dbh->do( 'INSERT INTO timelog (jobid, timestamp, percent)' .
+                  ' VALUES (?,?,?)', undef, $jobid, time, $percent );
+        &reload_jobs();
+        return &print_status;
+    }
+
+    # figure out what went wrong
+    warn "Could not save job: no job name given.\n" unless $jobname;
+    warn "Could not save job: invalid frequency.\n" if $freq !~ /^\d+$/;
+    warn "Could not save job: invalid urgency.\n" if $urg !~ /^\d+$/;
+    warn "Could not save job: invalid current total.\n" if $curr !~ /^\d+$/;
+    &main_prompt();
+}
+
+sub prompt_region {
+    my ( $regid ) = @_;
+    my $regp = $regid // 'n';
+
+    return $regid || $region if $region and $regnames{$region}; # constrained
+
+    print "Currently defined regions:\n";
+    my $rlist = 'Enter your choice -';
+    foreach my $r ( sort keys %regnames ) {
+        printf " %2s: %s\n", $r, $regnames{$r};
+        $rlist .= " $r,";
+    }
+    print "\n$rlist or 'n' for new region.\n";
+    my $regsel = &print_prompt( "[$regp]> " ) || $regp;
+
+    if ( $regsel =~ /^n/i ) {
+        my $newreg = &print_prompt( "New region name: " );
+        unless ( &is_unique_regname( $newreg ) ) {
+            print "There is already a region with that name.\n\n";
+            return &prompt_region( @_ );
+        }
+        $dbh->do( 'INSERT INTO regions (regname) VALUES (?)', undef, $newreg );
+        # update %regnames
+        my $reg = $dbh->selectall_arrayref( 'SELECT * FROM regions', { Slice => {} } );
+        %regnames = map { $_->{regid} => $_->{regname} } @$reg;
+        my %regids = map { $_->{regname} => $_->{regid} } @$reg;
+        return $regids{$newreg};
+
+    } elsif ( $regnames{$regsel} ) {
+        return &rename_region( $regsel );
+    } else {
+        print "That is not a valid region ID.\n\n";
+        &prompt_region( @_ );
+    }
+}
+
+sub rename_region {
+    my ( $regid ) = @_;
+    return unless defined $regid;
+
+    print "You may rename this region, or press RETURN to confirm the current name.\n";
+    my $newname = &print_prompt( "[$regnames{$regid}]> " );
+    if ( defined $newname and length $newname ) {
+        unless ( &is_unique_regname( $newname ) || lc $newname eq lc $regnames{$regid} ) {
+            print "There is already a region with that name.\n\n";
+            return &rename_region( @_ );
+        }
+        $dbh->do( 'UPDATE regions SET regname=? WHERE regid=?', undef, $newname, $regid );
+        # update %regnames
+        $regnames{$regid} = $newname;
+    } else {
+        return $regid;
+    }
+    # keep prompting until they accept the name
+    &rename_region( @_ );
+}
+
+sub is_unique_regname {
+    my ( $reg ) = @_;
+    return unless defined $reg and length $reg;
+
+    my %regids = map { lc $regnames{$_} => $_ } keys %regnames;
+    return $regids{lc $reg} ? 0 : 1;
+}
